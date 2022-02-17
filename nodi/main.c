93f6c262b90c1b437092e758ea91a0770819008e
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2022-02-17 12:55:58 +0400

    Add files via upload

--- nodi/main.c
+++ nodi/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-struct Player {
+typedef struct {
 	char* name;
 	int age;
 	int awake;
-};
+} Player;
 
-void sleep(struct Player* p) {
+void sleep(Player* p) {
 	p->awake = 0;
 	printf("%s is sleeping!\n", p->name);
 }
 
-void wake(struct Player* p) {
+void wake(Player* p) {
 	p->awake = 1;
 	printf("%s woke up!\n", p->name);
 }
 
 int main() {
-	struct Player nodi = {"Nodi", 9, 1};
+	Player nodi = {"Nodi", 9, 1};
 
 	sleep(&nodi);
 	printf("Awake: %d\n", nodi.awake);
